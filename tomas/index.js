79984bf500f62839979f0ad5d718da2817670b64
Tomas <eliz.tomas23@gmail.com>
2020-10-06 14:07:34 +0200

    variables declared and printed

---
+++ tomas/index.js
@@ -0,0 +1,15 @@
+let stringVar = 'some text';
+console.log(stringVar);
+
+// Print the following: "name is age in year", using your declared variables
+let name = 'Alex DeLarge';
+let age = 17;
+let year = 2032;
+console.log(name + ' is ' + age + ' in ' + year + '.');
+
+let isMarried = false;
+console.log(name + ' is married: ' + isMarried);
+
+let programmingLanguage = 'JavaScript';
+let isFun = true;
+console.log(programmingLanguage + ' is fun: ' + isFun);
