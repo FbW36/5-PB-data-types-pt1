12e0ce1172d5b921f6f05ae2198e2f01bdd48b1a
Cindy <Punk.c@hotmail.com>
2020-10-06 14:50:10 +0200

    add 8 data types

---
+++ cindy/index.js
@@ -0,0 +1,28 @@
+//1. Declare a variable and assign a string as a value. Make sure the string is at least 6 characters long. Print the variable to the console.
+let animal = "white Cat";
+console.log(animal);
+
+//2. Declare a variable called name and assign a string as a value.
+let name = "Ashe Fredjord";
+console.log(name);
+
+//3. Declare a variable called age and assign a number as a value.
+let age = 30;
+console.log(age);
+//4. Declare a variable called year and assign a number as a value.
+let year = 2020;
+console.log(year);
+
+//5. Print the following: "*name* is *age* in *year*", using your declared variables.
+console.log(name + " is " + age + " in " + year);
+
+//6. Declare a variable called isMarried and assign a boolean value.
+let isMarried = true;
+
+//7. Print the following "*name* is married: true/false", using your declared variables.
+console.log(name, isMarried ? "is Married" : "Not Married");
+
+//8. Declare a variable called programmingLanguage and assign the string "JavaScript". Declare a variable called isFun and give it a value of true.  Print "JavaScript is fun: true" to the console, using your declared variables.
+let programmingLanguage = "JavaScript";
+let isFun = true;
+console.log(programmingLanguage + " is fun : " + isFun);
