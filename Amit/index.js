52b3ec84f6bc2add78c346b2cbe18db66dafa24b
Amit833 <amitmajumder833@gmail.com>
2020-10-06 14:39:56 +0200

    Please check the 2nd exe.

---
+++ Amit/index.js
@@ -0,0 +1,22 @@
+// 1
+let country = "Bangladesh";
+console.log(country);
+
+// 2,3,4
+let name = "Amit Majumder";
+let age = 28;
+let year = 2020;
+
+//5
+let res1 = "  name is " + name + ". " + age + " in " + year;
+console.log(res1);
+
+// 6,7
+let isMarried = false;
+let res2 = " name is " + name + " married:true/" + isMarried;
+console.log(res2);
+
+//8
+let programmingLanguage = "JavaScript";
+let isFun = true;
+console.log(programmingLanguage + " is fun: " + isFun);
