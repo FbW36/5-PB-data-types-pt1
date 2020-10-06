b6dd0c755cb186163dc2a41705dae000f4081131
Sezai Keskin <sezai.keskin@me.com>
2020-10-06 13:56:16 +0200

    ex2

---
+++ Sezai/index.js
@@ -0,0 +1,29 @@
+// 1.
+
+let var1 = "dimebag";
+console.log(var1);
+
+// 2.
+let varName = "Sezai";
+
+// 3.
+let varAge = 36;
+
+// 4.
+let varYear = 2020
+
+// 5.
+console.log(varName, " is ",
+    varAge, " in ",
+    varYear);
+
+// 6.
+let varMarry = false;
+
+// 7.
+console.log(varName, " is married ", varMarry);
+
+// 8.
+let progLang = "Javascript";
+let isFun = true;
+console.log(progLang, " is fun ", isFun);
