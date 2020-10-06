4e9f07b4f22d371067c3b745640fb3a1a885a62b
Enikő Veszelka <encsooo@gmail.com>
2020-10-06 13:57:01 +0200

    Eniko done

---
+++ Eniko/index.js
@@ -0,0 +1,17 @@
+//1
+let num1 = 129456;
+console.log(num1);
+//2-5
+let name = 'Ella';
+let age = 2;
+let year = 2018;
+console.log(name + " is " + age + " in " + year);
+
+//6-7
+let isMarried = false;
+console.log(name + " is married: " + isMarried);
+
+//7
+let programmingLanguage = 'JavaScript';
+let isFun = true;
+console.log(programmingLanguage + " is fun: " + isFun);
