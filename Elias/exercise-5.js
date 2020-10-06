40e727fc15c0c10301aa537262afc07b42539eb5
Elias Guderian <46761652+GuderianE@users.noreply.github.com>
2020-10-06 14:51:06 +0200

    finsihed task

---
+++ Elias/exercise-5.js
@@ -0,0 +1,18 @@
+let myString = "longword";
+console.log(myString);
+
+
+let name = "Carola";
+let age = 200;
+let year = 1820;
+
+console.log(name + " is " + age + " in " + year);
+
+let isMarried = true;
+
+console.log(name + ' is married: ' + isMarried);
+
+let programmingLanguage = "JavaScript";
+let isFun = true;
+
+console.log(programmingLanguage + " is fun: " + isFun);
