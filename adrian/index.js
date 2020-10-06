925f77f0377e880ef24ad7f15a2d1f88b86e2d9c
AdrianValdes <adrianvaldeshdz@.gmail.com>
2020-10-06 14:53:39 +0200

    First commit

---
+++ adrian/index.js
@@ -0,0 +1,27 @@
+//1
+
+let a = 'this is a string';
+console.log(a);
+
+//2
+let name = 'Robert Müsil';
+
+//3
+let age = 1;
+
+//4
+let year = 1901;
+
+//5
+console.log(`${name} is ${age} in ${1901}`);
+
+//6
+let isMarried = false;
+
+//7
+console.log(`${name} is married: ${isMarried}`);
+
+//8
+let programmingLanguage = 'JavaScript';
+let isFun = true;
+console.log(`${programmingLanguage} is fun: ${isFun}`);
