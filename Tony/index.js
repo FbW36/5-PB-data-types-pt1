0861674a766858723ec1a6a152efd367a37ce853
Tony Tanaka <tony.h.tanaka@gmail.com>
2020-10-06 14:17:26 +0200

    finished exercise

---
+++ Tony/index.js
@@ -0,0 +1,26 @@
+// exercise 1
+let variable1 = "this is a string";
+console.log(variable1);
+
+// exercise 2
+let name = "Tony";
+
+// exercise 3
+let age = 38;
+
+// exercise 4
+let year = 2020;
+
+// exercise 5
+console.log(name, "is", age, "in", year);
+
+// exercise 6
+let isMarried = true;
+
+// exercise 7
+console.log(name, "is married:", isMarried);
+
+// exercise 8
+let programmingLanguage = "JavaScript";
+let isFun = true;
+console.log(programmingLanguage, "is fun:", isFun);
