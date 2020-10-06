3f788c170ea5a1dcdaf340402eba99803fc69b16
carveler <3heyaberlin@gmail.com>
2020-10-06 14:57:57 +0200

    mami

---
+++ mami/script.js
@@ -0,0 +1,15 @@
+var sport = "surfing";
+console.log(sport);
+
+var name = "john";
+var age = 30;
+var year = 2020;
+
+console.log(name + " is " + age + " in " + year);
+
+var isMarried = false;
+console.log(name + " is married: " + isMarried);
+
+var programmingLanguage = "Javascript";
+var isFun = true;
+console.log(programmingLanguage + " is Fun: " + isFun);
