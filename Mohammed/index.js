7cc87284dfb8cb2bec3fe404663266e0c9d8db4e
mashraee <m.almashraee@gmail.com>
2020-10-06 15:00:38 +0200

    exercise -5

---
+++ Mohammed/index.js
@@ -0,0 +1,29 @@
+
+// 1.
+let str = 'Mohammed';
+console.log(str);
+
+// 2.
+let name = "Maxmallian";
+
+// 3.
+let age = 31;
+
+// 4.
+let year = "06.10.2020";
+
+// 5.
+console.log("The name is " + name + " and the age is "+ age + " in "+ year);
+
+// 6. 
+let isMarried = true;
+
+// 7. 
+let status = isMarried ? (name + " is Married") : (name + " is not Married");
+console.log(status);
+
+// 8.
+let isFun = true;
+let programmingLanguage = "JavaScript";
+let fun = isFun ? (programmingLanguage + " is fun: "+ isFun) : (null) ;
+console.log(fun);
