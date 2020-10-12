bbf51d5a7217346220cb6c7629182d4e449e9125
Zarzarius <zarzarius@gmail.com>
2020-10-12 12:37:37 +0200

    exercise done

--- Azael/index.js
+++ Azael/index.js
@@ -2,16 +2,35 @@
 
 let string = "ipa is delicious";
 
+console.log("string ==>" + string);
+
 // 2. Declare a variable called name and assign a string as a value.
+
+let name = "Azael";
 
 // 3. Declare a variable called age and assign a number as a value.
 
+let age = "34";
+
 // 4. Declare a variable called year and assign a number as a value.
+
+let year = "1986";
 
 // 5. Print the following: "*name* is *age* in *year*", using your declared variables.
 
+console.log(name + " " + "is" + " " + age + " " + "in" + " " + year);
+
 // 6. Declare a variable called isMarried and assign a boolean value.
+let isMarried = false;
 
 // 7. Print the following "*name* is married: true/false", using your declared variables.
 
+console.log(name + " " + "is" + " " + "married:" + " " + isMarried);
+
 // 8. Declare a variable called programmingLanguage and assign the string "JavaScript". Declare a variable called isFun and give it a value of true.  Print "JavaScript is fun: true" to the console, using your declared variables.
+
+let programmingLanguage = "JavaScript";
+
+let isFun = true;
+
+console.log(programmingLanguage + " " + "is" + " " + "fun:" + " " + isFun);
