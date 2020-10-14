746b1db2cae6e9bf600daac08c7c27dcee0b158f
Aneudy <mailaneudygarcia@gmail.com>
2020-10-14 16:31:07 +0200

    done

---
+++ Aneudy/Homework-Tuesday-DataTypes.js
@@ -0,0 +1,25 @@
+
+//1. Declare a variable and assign a string as a value. Make sure the string is at least 6 characters long. Print the variable to the console.
+var one = "Marco Hunter";
+console.log(one);
+//2. Declare a variable called name and assign a string as a value. 
+var name = "Aneudy";
+//3. Declare a variable called age and assign a number as a value. 
+var age = 6;
+//4. Declare a variable called year and assign a number as a value. 
+var year = 1980;
+//5. Print the following: "*name* is *age* in *year*", using your declared variables. 
+console.log(name, age, year);
+console.log(name + " is " + age + " in " + year +".");
+//6. Declare a variable called isMarried and assign a boolean value. 
+var isMarried = false;
+console.log(isMarried);
+//7. Print the following "*name* is married: true/false", using your declared variables.
+console.log(name + " is married: true/false ");
+//8. Declare a variable called programmingLanguage and assign the string "JavaScript". 
+var programmingLanguage = "JavaScript";
+//Declare a variable called isFun and give it a value of true. 
+var isFun = true;
+// Print "JavaScript is fun: true" to the console, using your declared variables. 
+console.log(programmingLanguage, isFun)
+console.log(programmingLanguage + " is fun: " + isFun )
